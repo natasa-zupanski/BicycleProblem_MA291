f84e68d90dde51621b7f8ee36108aa0241471218
natasa-zupanski <zupansnm@rose-hulman.edu>
2024-09-18 21:08:16 -0400

    inner classes for vars

--- Main.java
+++ Main.java
@@ -1,6 +1,36 @@
 
 class Main {
 
+    private final class Constants {
+        final double gravity = 9.81;
+        final double kinematic_velocity_of_air = 0;
+        final double cross_section_spoked_wheel = 0;
+        final double cross_section_disc_wheel = 0;
+        final double radius_spoked_wheel = 0;
+        final double radius_disc_wheel = 0;
+    }
+
+    private class Constant_Per_Course {
+        double wind_speed;
+        double wind_direction;
+        double air_direction;
+        double mass_bike_and_rider;
+        double cross_section_rider_and_bike;
+        int power;
+    }
+
+    private class Var_Per_Instance {
+        double grade;
+        double bike_direction;
+    }
+
+    private class Inputs_Calculation {
+        double step_length;
+        double number_steps;
+        Var_Per_Instance[] situations;
+        Constant_Per_Course course_specs;
+    }
+
     public static void main(String[] args) {
 
     }
